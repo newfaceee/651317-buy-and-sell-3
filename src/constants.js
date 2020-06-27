d619a37fca1034c1a36731aaf2285ac4032db671
Edem Reshidov <newfaceof@yandex.ru>
2020-06-28 00:29:39 +0300

    Создает файл с константами

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const USER_ARGV_INDEX = 2;
+const DEFAULT_COMMAND = `--help`;
+const ExitCode = {
+  SUCCESS: 0,
+  ERROR: 1,
+};
+
+module.exports = {
+  USER_ARGV_INDEX,
+  DEFAULT_COMMAND,
+  ExitCode,
+};
