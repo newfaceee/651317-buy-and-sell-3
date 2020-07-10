088fd5d1656e7161ba286504ebf80cf79855131a
Edem Reshidov <newfaceof@yandex.ru>
2020-07-10 09:15:46 +0300

    Добавляет команду --server в cli

---
+++ src/service/cli/server.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const chalk = require(`chalk`);
+const fs = require(`fs`).promises;
+const http = require(`http`);
+
+const {ExitCode, HttpCode} = require(`../../constants`);
+
+const DEFAULT_PORT = 3000;
+const FILE_NAME = `mocks.json`;
+
+const sendResponse = (res, statusCode, message) => {
+  const template = `
+    <!Doctype html>
+      <html lang="ru">
+        <head>
+          <title>With love from Node</title>
+        </head>
+        <body>
+          ${message}
+        </body>
+      </html>`.trim();
+  res.statusCode = statusCode;
+  res.writeHead(statusCode, {
+    'Content-Type': `text/html; charset=UTF-8`
+  });
+  res.end(template);
+};
+
+const onClientConnect = async (req, res) => {
+  const notFoundMessageText = `Not Found`;
+
+  switch (req.url) {
+    case `/`:
+      try {
+        const fileContent = await fs.readFile(FILE_NAME);
+        const mocks = JSON.parse(fileContent);
+        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
+        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
+      } catch (err) {
+        console.log(err);
+        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      }
+      break;
+    default:
+      console.log('da');
+      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      break;
+  }
+};
+
+module.exports = {
+  name: `--server`,
+  run(args) {
+    const [customPort] = args;
+    const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
+
+    http.createServer(onClientConnect)
+    .listen(port)
+    .on(`listening`, (err) => {
+      if (err) {
+        console.error(`Error while creating server`, err);
+        return process.exit(ExitCode.ERROR);
+      }
+      return console.info(chalk.green(`Waiting for connection on port: ${port}`));
+    });
+
+  }
+};
