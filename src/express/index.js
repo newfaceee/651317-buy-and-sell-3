1b457c5d45d073b7ea83eebe708913fd898343b8
Edem Reshidov <newfaceof@yandex.ru>
2020-07-13 21:06:15 +0300

    Устанавливает шаблонизатор Pug и добавляет статичные шаблоны для основных страниц

--- src/express/index.js
+++ src/express/index.js
@@ -1,13 +1,18 @@
 'use strict';
 const express = require(`express`);
+const path = require(`path`);
 
 const offerRoutes = require(`./routes/offers-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
 const DEFAULT_PORT = 8000;
+const PUBLIC_DIR = `public`;
 
 const app = express();
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.use(`/`, mainRoutes);
 app.use(`/offers`, offerRoutes);
