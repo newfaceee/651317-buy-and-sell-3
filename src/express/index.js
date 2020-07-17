c8ebccf6a36576effdaff26850a951590204c6b6
Edem Reshidov <newfaceof@yandex.ru>
2020-07-17 15:03:42 +0300

    Добавляет обработку 404 ошибки и 500 ошибки

--- src/express/index.js
+++ src/express/index.js
@@ -6,8 +6,7 @@ const offerRoutes = require(`./routes/offers-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
-const DEFAULT_PORT = 8000;
-const PUBLIC_DIR = `public`;
+const {HttpCode, DEFAULT_PORT, PUBLIC_DIR} = require(`../constants`);
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
@@ -17,4 +16,13 @@ app.set(`view engine`, `pug`);
 app.use(`/`, mainRoutes);
 app.use(`/offers`, offerRoutes);
 app.use(`/my`, myRoutes);
+
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND).render(`./errors/404`);
+});
+
+app.use((req, res) => {
+  res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`./erroes/500`);
+});
+
 app.listen(DEFAULT_PORT);
